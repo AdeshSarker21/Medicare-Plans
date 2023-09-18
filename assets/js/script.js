1cead207291f350bf18d6fa635a0e857cd2b7cc3
AdeshSarker21 <adeshchandrosarker.sdp@gmail.com>
2023-09-18 23:26:24 +0600

    Final Project

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#submit-btn").click(function(){
+        $(".popup-section, .effect").show();
+    });
+    $(".close").click(function(){
+        $(".popup-section, .effect").hide();
+    })
+
+    });
